21f0c135d7038c5898b3851063887dc3fc1324ff
oh hyun seok <hyunsokstar@gmail.com>
2026-02-18 09:32:52 +0900

    Add @MockitoBean AuthMapper to FnbMiniApplicationTests to avoid DB connection during test

--- fnb-mini-back/src/test/java/com/cj/fnbmini/FnbMiniApplicationTests.java
+++ fnb-mini-back/src/test/java/com/cj/fnbmini/FnbMiniApplicationTests.java
@@ -1,10 +1,15 @@
 package com.cj.fnbmini;
 
+import com.cj.fnbmini.auth.AuthMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class FnbMiniApplicationTests {
+
+	@MockitoBean
+	AuthMapper authMapper;
 
 	@Test
 	void contextLoads() {
