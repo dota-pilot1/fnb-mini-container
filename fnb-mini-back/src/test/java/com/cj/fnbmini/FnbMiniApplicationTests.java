6cecd5db81fbad16b945eae263c8f7a7ab23edcb
oh hyun seok <hyunsokstar@gmail.com>
2026-02-18 09:33:33 +0900

    Mock all Mapper beans and disable SQL init for offline context load test

--- fnb-mini-back/src/test/java/com/cj/fnbmini/FnbMiniApplicationTests.java
+++ fnb-mini-back/src/test/java/com/cj/fnbmini/FnbMiniApplicationTests.java
@@ -1,6 +1,8 @@
 package com.cj.fnbmini;
 
 import com.cj.fnbmini.auth.AuthMapper;
+import com.cj.fnbmini.brand.BrandMapper;
+import com.cj.fnbmini.settlement.SettlementMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
@@ -10,6 +12,12 @@ class FnbMiniApplicationTests {
 
 	@MockitoBean
 	AuthMapper authMapper;
+
+	@MockitoBean
+	BrandMapper brandMapper;
+
+	@MockitoBean
+	SettlementMapper settlementMapper;
 
 	@Test
 	void contextLoads() {
